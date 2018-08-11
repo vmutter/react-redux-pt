e05d199615e1e62be7978f0902f6b68957b99251
Vinicius Mutter Erbetta <vinicius.me@gmail.com>
2018-08-11 08:34:12 -0300

    Adicionando o Preset do React

--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_module/,
         query: {
-          presets: ['es2015'],
+          presets: ['es2015', 'react'],
           plugins: ['transform-object-rest-spread']
         }
     }]
