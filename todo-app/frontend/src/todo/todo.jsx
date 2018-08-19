31a2c965578f5b464e18e787004dd9a784b58097
Vinicius Mutter Erbetta <vinicius.me@gmail.com>
2018-08-19 07:15:07 -0300

    57. Evento Adicionar (Integração Backend)

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class extends Component {
 
@@ -21,7 +25,9 @@ export default class extends Component {
     }
 
     handleAdd() {
-        console.log(this)
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('Funcionou!'))
     }
 
     render() {
