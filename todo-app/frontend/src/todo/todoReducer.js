ff9c70d46e35bea45f41cda19b01e207fdb2982f
Vinicius Mutter Erbetta <vinicius.me@gmail.com>
2018-08-25 10:58:43 -0300

    79. Aplicando Middleware (Redux-Promise)

--- todo-app/frontend/src/todo/todoReducer.js
+++ todo-app/frontend/src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Reunião',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Consulta',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default function todoReducer(state = INITIAL_STATE, action) {
