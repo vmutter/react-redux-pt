d8d95ed219a055778abea52ed6b14d9d5079fdec
Vinicius Mutter Erbetta <vinicius.me@gmail.com>
2018-09-01 10:47:17 -0300

    96. Obtendo o Sumário de Pagamentos

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -6,9 +6,26 @@ BillingCycle.updateOptions({new: true, runValidators: true})
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if (error) {
-            res.status(500).json({errors: [erro]})
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
         }
-        res.json({value})
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate({
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+    }, {
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, {
+        $project: {_id: 0, credit: 1, debt: 1}
+    }, (error, result) => {
+        if (error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
     })
 })
 
